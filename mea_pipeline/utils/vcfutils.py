4b1b9b9b28008875eec813c13b38ab7521058f4f
anto @ alrescha <trimarsanto@gmail.com>
2024-08-03 08:16:47 +0930

    update: change flow to use set_genotypes()

--- mea_pipeline/utils/vcfutils.py
+++ mea_pipeline/utils/vcfutils.py
@@ -12,11 +12,23 @@ from cyvcf2 import VCF, Writer
 from mea_pipeline import cout, cerr, cexit
 
 
+def set_genotypes(variant, indexes, value):
+    for idx in indexes:
+        variant.genotypes[indexes] = value
+
+
+# after setting GT with this function, it is advised to pipe the result to:
+#   bcftools +fill-tags -o {output} - -- -t AC,AC_Hom,AC_Het,AF,AN,AF_MISSING,MAF,NS,TYPE
+# to recalculate parameters in INFO field (since bcftools might compute
+# faster than python-based like this function)
+
+
 def set_GT(
     infile: str | pathlib.Path,
     outfile: str | pathlib.Path,
     *,
     logfile: str | pathlib.Path | None = None,
+    minimum_depth: int = -1,
     minimum_minor_depth: int = -1,
     minimum_minor_ratio: float = -1,
     set_het_to_ref: bool = False,
@@ -27,13 +39,26 @@ def set_GT(
     set_missing_to_alt: bool = False,
     set_id: bool = False,
     headers: list = [],
+    threads: int = 1,
 ):
 
-    # after setting GT with this function, it is advised to run:
-    # bcftools +setGT -- -t q -n . -i "FORMAT/DP<{mindepth}"
-    # to set min depth and recalculate parameters in INFO field
+    # we rely that cyvcf2.Variant().gt_types is (gts012=False):
+    #   0 for 0/0
+    #   1 for any hets such as 0/1, 0/2, 1/2, etc
+    #   2 for ./.
+    #   3 for 1/1, 2/2, etc
 
-    vcf = VCF(infile)
+    gt_HOM_REF = 0
+    gt_HET = 1
+    gt_MISSING = 2
+    gt_HOM_ALT = 3
+
+    genotype_HOM_REF = [0, 0, False]
+    genotype_HET = [0, 1, False]
+    genotype_MISSING = [-1, -1, False]
+    genotype_HOM_ALT = [1, 1, False]
+
+    vcf = VCF(infile, threads=threads)
     w = Writer(outfile, vcf)
 
     for header in headers:
@@ -41,11 +66,21 @@ def set_GT(
 
     logs = []
 
+    # prepare some flags
+    USE_GT = True
+    if minimum_minor_depth >= 0 or minimum_minor_ratio >= 0:
+        if minimum_depth < 0:
+            raise ValueError(
+                "when setting for minimum_minor_depth or minimum_minor_ratio, "
+                "minimum_depth also needs to be set"
+            )
+        USE_GT = False
+
     for v in vcf:
 
         AD = v.format("AD")
 
-        if minimum_minor_depth >= 0 or minimum_minor_ratio >= 0:
+        if not USE_GT:
 
             if len(v.ALT) == 1:
                 # we have biallelic alleles
@@ -80,7 +115,9 @@ def set_GT(
                     # minor allele with minor_args[1] which is guarantee to be
                     # smaller allele index than minor_args[2] when the depths of
                     # those minor alleles are the same (by np.argsort(-AD))
-                    minor_alleles[equal_minor_depths] = minor_args[equal_minor_depths, 1]
+                    minor_alleles[equal_minor_depths] = minor_args[
+                        equal_minor_depths, 1
+                    ]
 
                     # get two of the most common allele index
                     allele_indexes = (AD < minimum_minor_depth).sum(axis=0).argsort()
@@ -114,42 +151,52 @@ def set_GT(
                 allele = major_alleles[idx]
                 v.genotypes[idx] = [allele, allele, False]
 
+            # hets are those that are not non-hets
+            hets = (~non_hets).nonzero()[0]
+
         else:
             # get non-hets from genotypes / GT fields
-            # XXX continue here
-            non_hets = []
 
-        # for all that are not non-hets, eg. the hets, set their alleles
-        for idx in (~non_hets).nonzero()[0]:
-            if set_het_to_ref:
-                v.genotypes[idx] = [0, 0, False]
-                continue
-            if set_het_to_alt:
-                # WARNING: for now this only correct for biallelic variants
-                v.genotypes[idx] = [1, 1, False]
-                continue
-            if set_het_to_missing:
-                v.genotypes[idx] = [-1, -1, False]
-                continue
-            # sort alleles
-            if (maj_allele := major_alleles[idx]) > (min_allele := minor_alleles[idx]):
-                alleles = [min_allele, maj_allele, False]
-            else:
-                alleles = [maj_allele, min_allele, False]
-            v.genotypes[idx] = alleles
+            hets = v.gt_types != gt_HET
+            non_hets = (v.gt_types == gt_HOM_REF) | (v.gt_types == gt_HOM_ALT)
 
-        for idx in (v.gt_depths == 0).nonzero()[0]:
-            if set_missing_to_ref:
-                v.genotypes[idx] = [0, 0, False]
-                continue
-            if set_missing_to_alt:
-                # WARNING: for now this only correct for biallelic variants
-                v.genotypes[idx] = [1, 1, False]
-                continue
-            if set_missing_to_het:
-                # WARNING: for now this only correct for biallelic variants
-                v.genotypes[idx] = [0, 1, False]
-            v.genotypes[idx] = [-1, -1, False]
+        # -- handling hets -- #
+
+        # for all hets, set their alleles
+        if set_het_to_ref:
+            set_genotypes(v, hets, genotype_HOM_REF)
+        elif set_het_to_alt:
+            set_genotypes(v, hets, genotype_HOM_ALT)
+        elif set_het_to_missing:
+            set_genotypes(v, hets, genotype_MISSING)
+        elif minimum_minor_depth >= 0 or minimum_minor_ratio >= 0:
+            for idx in hets:
+                # sort alleles to give 0/1, 0/2 or 1/2 instead of 1/0, 2/0 or 2/1
+                if (maj_allele := major_alleles[idx]) > (
+                    min_allele := minor_alleles[idx]
+                ):
+                    alleles = [min_allele, maj_allele, False]
+                else:
+                    alleles = [maj_allele, min_allele, False]
+                v.genotypes[idx] = alleles
+
+        # -- handling missing -- #
+
+        if minimum_depth >= 0:
+            missings = (v.gt_depths < minimum_depth).nonzero()[0]
+        else:
+            missings = v.gt_types == gt_MISSING
+
+        if set_missing_to_ref:
+            set_genotypes(v, missings, genotype_HOM_REF)
+        elif set_missing_to_alt:
+            # WARNING: this is correct only for biallelic variants
+            set_genotypes(v, missings, genotype_HOM_ALT)
+        elif set_missing_to_het:
+            # WARNING: this is correct only for biallelic variants
+            set_genotypes(v, missings, genotype_HET)
+        elif minimum_depth >= 0:
+            set_genotypes(v, missings, genotype_MISSING)
 
         # reset genotypes
         v.genotypes = v.genotypes
