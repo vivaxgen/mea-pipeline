566eae2fb6a05d349fd7c32a707381555acb3c54
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2025-08-29 17:16:37 +0930

    add: copyright

---
+++ mea_pipeline/cmds/gwas_run_assoc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env mea-pl
+# [https://github.com/vivaxgen/mea-pipeline]
+
+__copyright__ = "(c) 2024, Hidayat Trimarsanto <trimarsanto@gmail.com>"
+__license__ = "MIT"
+
+
+from mea_pipeline import arg_parser, cerr
